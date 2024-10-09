ed3941ec4add81bc4e16519392fc80fffd01614f
Jono Sommers <jonosommers95@gmail.com>
2024-10-09 09:55:59 -0500

    Backtracking

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -10,16 +10,27 @@ describe Deck do
             expect(deck.cards).to eq([])
         end
     end
-
-    describe 'add_cards' do
-        it 'should be able to add cards to the cards array' do
-            deck = Deck.new([])
-
-            card1 = deck.add_card(:diamond, 'Queen', 12)
-            card2 = deck.add_card(:spade, '3', 3)
-            card3 = deck.add_card(:heart, 'Ace', 14)
-
-            expect(deck.cards).to eq([card1, card2, card3])
-        end
-    end
 end
+
+
+
+
+
+
+
+
+
+
+
+#     describe 'add_cards' do
+#         it 'should be able to add cards to the cards array' do
+#             deck = Deck.new([])
+
+#             card1 = deck.add_card(:diamond, 'Queen', 12)
+#             card2 = deck.add_card(:spade, '3', 3)
+#             card3 = deck.add_card(:heart, 'Ace', 14)
+
+#             expect(deck.cards).to eq([card1, card2, card3])
+#         end
+#     end
+# end
