cbeab09ad9b02739b03ebc78bbbec96912349b22
Jono Sommers <jonosommers95@gmail.com>
2024-10-10 17:53:44 -0500

    percent high ranking method works

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,7 +1,6 @@
 require 'rspec'
 require './lib/card'
 require './lib/deck'
-require 'pry'
 
 RSpec.describe Deck do
     it 'exists' do
@@ -24,6 +23,16 @@ RSpec.describe Deck do
         expect(deck.rank_of_card_at(0)).to eq(12)
     end
 
+    it 'returns rank of card at specified index position' do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)    
+        card3 = Card.new(:heart, 'Ace', 14)
+        cards = [card1, card2, card3]
+
+        deck = Deck.new(cards)
+        expect(deck.rank_of_card_at(2)).to eq(14)
+    end
+
     it 'returns an array of cards with rankings >= 11' do
         card1 = Card.new(:diamond, 'Queen', 12)
         card2 = Card.new(:spade, '3', 3)    
@@ -33,6 +42,17 @@ RSpec.describe Deck do
         deck = Deck.new(cards)
         expect(deck.high_ranking_cards).to eq([card1, card3])
     end
+
+    it 'returns a percentage of cards that are high ranking' do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)    
+        card3 = Card.new(:heart, 'Ace', 14)
+        cards = [card1, card2, card3]
+
+        deck = Deck.new(cards)
+        expect(deck.percent_high_ranking).to eq(66.67)
+    end
+
 end
 
 
