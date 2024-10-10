1e0b27582d06e874d8e653c18614f5e8a1cad255
Jono Sommers <jonosommers95@gmail.com>
2024-10-10 13:08:30 -0500

    Working on high_ranking_cards test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,11 +1,12 @@
 require 'rspec'
 require './lib/card'
 require './lib/deck'
+require 'pry'
 
 RSpec.describe Deck do
     it 'exists' do
         card1 = Card.new(:diamond, 'Queen', 12)
-        card2 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)    
         card3 = Card.new(:heart, 'Ace', 14)
         cards = [card1, card2, card3]
 
@@ -15,12 +16,22 @@ RSpec.describe Deck do
 
     it 'returns rank of card at specified index position' do
         card1 = Card.new(:diamond, 'Queen', 12)
-        card2 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)    
         card3 = Card.new(:heart, 'Ace', 14)
         cards = [card1, card2, card3]
 
         deck = Deck.new(cards)
         expect(deck.rank_of_card_at(0)).to eq(12)
+    end
+
+    it 'returns an array of cards with rankings >= 11' do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)    
+        card3 = Card.new(:heart, 'Ace', 14)
+        cards = [card1, card2, card3]
+
+        deck = Deck.new(cards)
+        expect(deck.high_ranking_cards).to eq([card1, card3])
     end
 end
 
