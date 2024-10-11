532802ca16531efa34bb289d72be849a9cace873
Jono Sommers <jonosommers95@gmail.com>
2024-10-10 20:17:21 -0500

    All tests passing in player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,14 @@
 class Player
 
-    attr_reader :name
+    attr_reader :name, :deck
 
-    def initialize(name)
+    def initialize(name, deck)
         @name = name
+        @deck = deck
+    end
+
+    def has_lost?
+        @deck.cards.empty?
     end
 
 end
