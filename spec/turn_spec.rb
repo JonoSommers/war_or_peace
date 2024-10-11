5777c9f647406bc092518aa50a21c50e5a5d9460
Jono Sommers <jonosommers95@gmail.com>
2024-10-11 18:57:34 -0500

    All tests passing but last one

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -22,6 +22,7 @@ RSpec.describe Turn do
     player2 = Player.new("Aurora", deck2) 
 
     turn = Turn.new(player1, player2)
+
     expect(turn.player1).to eq(player1)
     expect(turn.player2).to eq(player2)
     end
@@ -44,6 +45,7 @@ RSpec.describe Turn do
         
     
         turn = Turn.new(player1, player2)
+
         expect(turn.spoils_of_war).to eq([])
     end
 
@@ -65,6 +67,78 @@ RSpec.describe Turn do
         
     
         turn = Turn.new(player1, player2)
+
         expect(turn.type).to eq(:basic)
+    end
+
+    it 'determines the winner of the turn' do
+        card1 = Card.new(:heart, 'Jack', 11)
+        card2 = Card.new(:heart, '10', 10)
+        card3 = Card.new(:heart, '9', 9)
+        card4 = Card.new(:diamond, 'Jack', 11)
+        card5 = Card.new(:heart, '8', 8)
+        card6 = Card.new(:diamond, 'Queen', 12)
+        card7 = Card.new(:heart, '3', 3)
+        card8 = Card.new(:diamond, '2', 2)
+    
+        deck1 = Deck.new([card1, card2, card5, card8])
+        deck2 = Deck.new([card3, card4, card6, card7])
+    
+        player1 = Player.new("Megan", deck1)
+        player2 = Player.new("Aurora", deck2)
+
+        turn = Turn.new(player1, player2)
+        
+        expect(turn.winner).to eq(player1)
+    end
+
+    it 'will send cards from players decks to spoils_of_war' do
+        card1 = Card.new(:heart, 'Jack', 11)
+        card2 = Card.new(:heart, '10', 10)
+        card3 = Card.new(:heart, '9', 9)
+        card4 = Card.new(:diamond, 'Jack', 11)
+        card5 = Card.new(:heart, '8', 8)
+        card6 = Card.new(:diamond, 'Queen', 12)
+        card7 = Card.new(:heart, '3', 3)
+        card8 = Card.new(:diamond, '2', 2)
+
+        deck1 = Deck.new([card1, card2, card5, card8])
+        deck2 = Deck.new([card3, card4, card6, card7])
+
+        player1 = Player.new("Megan", deck1)
+        player2 = Player.new("Aurora", deck2)
+
+        turn = Turn.new(player1, player2)
+        turn.pile_of_cards
+
+        expect(turn.spoils_of_war).to eq([card1, card3])
+        expect(player1.deck.cards).to eq([card2, card5, card8])
+        expect(player2.deck.cards).to eq([card4, card6, card7])
+    end
+
+
+    it 'will add the cards from the @spoils_of_war array to the winner of the turns deck' do
+        card1 = Card.new(:heart, 'Jack', 11)
+        card2 = Card.new(:heart, '10', 10)
+        card3 = Card.new(:heart, '9', 9)
+        card4 = Card.new(:diamond, 'Jack', 11)
+        card5 = Card.new(:heart, '8', 8)
+        card6 = Card.new(:diamond, 'Queen', 12)
+        card7 = Card.new(:heart, '3', 3)
+        card8 = Card.new(:diamond, '2', 2)
+
+        deck1 = Deck.new([card1, card2, card5, card8])
+        deck2 = Deck.new([card3, card4, card6, card7])
+
+        player1 = Player.new("Megan", deck1)
+        player2 = Player.new("Aurora", deck2)
+
+        turn = Turn.new(player1, player2)
+        turn.pile_of_cards
+        winner = turn.winner
+        turn.award_spoils(winner)
+
+        expect(player1.deck).to eq([card2, card5, card8, card1, card3])
+        expect(player2.deck).to eq([card4, card6, card7])
     end
 end
