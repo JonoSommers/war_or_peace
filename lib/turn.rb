7f7629e66762bef5fca0f6b3fb642e7b6f2aa44f
Jono Sommers <jonosommers95@gmail.com>
2024-10-10 21:01:43 -0500

    Type test passes

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,10 +1,21 @@
 class Turn
 
-    attr_reader :player1, :player2
+    attr_reader :player1, :player2, :spoils_of_war
 
     def initialize(player1, player2)
         @player1 = player1
         @player2 = player2
+        @spoils_of_war = []
+    end
+
+    def type
+        if @player1.deck.cards[0].rank > @player2.deck.cards[0].rank
+            :basic
+        elsif @player2.deck.cards[0].rank > @player1.deck.cards[0].rank
+            :basic
+        else
+            @spoils_of_war
+        end
     end
 
 end
