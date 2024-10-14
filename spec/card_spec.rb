dccc5ac1b6d248d3c50205111d25000bc04cd500
Jono Sommers <jonosommers95@gmail.com>
2024-10-14 17:26:33 -0500

    Updated and organized tests

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -15,19 +15,4 @@ RSpec.describe Card do
     expect(card.value).to eq('Queen')
     expect(card.rank).to eq(12)
   end
-
-  it "exists" do
-    card = Card.new(:heart, 'Jack', 11)
-
-    expect(card).to be_an_instance_of(Card)
-  end
-
-  it "has readable attributes" do
-    card = Card.new(:heart, 'Jack', 11)
-
-    expect(card.suit).to eq(:heart)
-    expect(card.value).to eq('Jack')
-    expect(card.rank).to eq(11)
-  end
 end
-
