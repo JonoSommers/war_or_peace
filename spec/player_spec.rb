42eba462f3eab5003fda2eb9972c69a391b8a419
Jono Sommers <jonosommers95@gmail.com>
2024-10-14 17:27:12 -0500

    Updated and organized tests

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,24 +4,45 @@ require './lib/deck'
 require './lib/player'
 
 RSpec.describe Player do
-    it 'exists' do
-        card1 = Card.new(:diamond, 'Queen', 12)
-        card2 = Card.new(:spade, '3', 3)
-        card3 = Card.new(:heart, 'Ace', 14)
-        deck = Deck.new([card1, card2, card3])
+    describe "#initialize" do
+        it "exists" do
+            card1 = Card.new(:diamond, 'Queen', 12)
+            card2 = Card.new(:spade, '3', 3)
+            card3 = Card.new(:heart, 'Ace', 14)
+            deck = Deck.new([card1, card2, card3])
 
-        player = Player.new('Clarisa', deck)
-        expect(player.name).to eq('Clarisa')
-        expect(player.deck).to eq(deck)
+            player = Player.new('Clarisa', deck)
+            expect(player).to be_a(Player)
+        end
+
+        it 'has attributes' do
+            card1 = Card.new(:diamond, 'Queen', 12)
+            card2 = Card.new(:spade, '3', 3)
+            card3 = Card.new(:heart, 'Ace', 14)
+            deck = Deck.new([card1, card2, card3])
+
+            player = Player.new('Clarisa', deck)
+            expect(player.name).to eq('Clarisa')
+            expect(player.deck).to be_a(Deck)
+        end
     end
 
-    it 'determines if the player lost that roud' do
-        card1 = Card.new(:diamond, 'Queen', 12)
-        card2 = Card.new(:spade, '3', 3)
-        card3 = Card.new(:heart, 'Ace', 14)
-        deck = Deck.new([card1, card2, card3])
+    describe "#has_lost?" do
+        it 'determines if the player lost that round' do
+            card1 = Card.new(:diamond, 'Queen', 12)
+            card2 = Card.new(:spade, '3', 3)
+            card3 = Card.new(:heart, 'Ace', 14)
+            deck = Deck.new([card1, card2, card3])
 
-        player = Player.new('Clarisa', deck)
-        expect(player.has_lost?).to be(false)
+            player = Player.new('Clarisa', deck)
+            expect(player.has_lost?).to be(false)
+            player.deck.remove_card
+            expect(player.has_lost?).to be(false)
+            player.deck.remove_card
+            expect(player.has_lost?).to be(false)
+            player.deck.remove_card
+            expect(player.has_lost?).to be(true)
+            expect(player.deck.cards).to eq([])
+        end
     end
 end
