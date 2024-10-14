bc7dc4e7183c2c1ab3bc64d51b6dfb1b06bf70a9
Jono Sommers <jonosommers95@gmail.com>
2024-10-14 17:29:56 -0500

    Added extra test to high_ranking_cards

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -48,6 +48,16 @@ RSpec.describe Deck do
 
             deck = Deck.new(cards)
             expect(deck.high_ranking_cards).to eq([card1, card3])
+        end
+
+        it 'updates the array when a card is removed' do
+            card1 = Card.new(:diamond, 'Queen', 12)
+            card2 = Card.new(:spade, '3', 3)    
+            card3 = Card.new(:heart, 'Ace', 14)
+            cards = [card1, card2, card3]
+
+            deck = Deck.new(cards)
+            expect(deck.high_ranking_cards).to eq([card1, card3])
             deck.remove_card
             expect(deck.high_ranking_cards).to eq([card3])
         end
