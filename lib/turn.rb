747e2c8a5c626a9e222c18318458014cd1e9c9eb
Jono Sommers <jonosommers95@gmail.com>
2024-10-14 21:32:43 -0500

    All tests passing

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,45 +9,58 @@ class Turn
     end
 
     def type
-        if @player1.deck.cards[0].rank > @player2.deck.cards[0].rank
-            :basic
-        elsif @player2.deck.cards[0].rank > @player1.deck.cards[0].rank
-            :basic
-        elsif (@player1.deck.cards[0].rank == @player2.deck.cards[0].rank)
-            :war
-        else
-            (@player1.deck.cards[0].rank == @player2.deck.cards[0].rank) && (@player1.deck.cards.count >= 3 && @player2.deck.cards.count >= 3)
+        if (@player1.deck.cards[0].rank == @player2.deck.cards[0].rank) &&
+            (@player1.deck.cards[2].rank == @player2.deck.cards[2].rank)
             :mutually_assured_destruction
+        elsif 
+            (@player1.deck.cards[0].rank == @player2.deck.cards[0].rank)
+            (@player1.deck.cards.count >= 3 && @player2.deck.cards.count >= 3)
+            :war
+        elsif 
+            @player2.deck.cards[0].rank > @player1.deck.cards[0].rank
+            :basic
+        else
+            @player1.deck.cards[0].rank > @player2.deck.cards[0].rank
+            :basic
         end
     end
 
     def winner
-        if @player1.deck.cards[0].rank > @player2.deck.cards[0].rank
-            @player1
-        elsif
-            @player2.deck.cards[0].rank > @player1.deck.cards[0].rank
-            @player2
-        elsif
-            (@player1.deck.cards[0].rank == @player2.deck.cards[0].rank) && (@player1.deck.cards.count >= 3 && @player2.deck.cards.count >= 3)
-            @spoils_of_war.push(@player1.deck.cards[0, 1], @player2.deck.cards[0, 1])
-            @player1.deck.cards[0].rank > @player2.deck.cards[0].rank
-            @player1
-        elsif
-            (@player2.deck.cards[0].rank == @player1.deck.cards[0].rank) && (@player2.deck.cards.count >= 3) && (@player1.deck.cards.count >= 3)
-            @spoils_of_war.push(@player2.deck.cards[0, 1], @player1.deck.cards[0, 1])
-            @player2.deck.cards[0].rank > @player1.deck.cards[0].rank
-            @player2
-        elsif
-            (@player1.deck.cards[0].rank == @player2.deck.cards[0].rank) && 
+        if (@player1.deck.cards[0].rank == @player2.deck.cards[0].rank) && 
             (@player1.deck.cards[2].rank == @player2.deck.cards[2].rank)
             "No Winner"
+        elsif
+            @player1.deck.cards[0].rank == @player2.deck.cards[0].rank && 
+            @player1.deck.cards.count >= 3 && @player2.deck.cards.count >= 3 &&
+            @player1.deck.cards[2].rank > @player2.deck.cards[2].rank
+            @player1
+        elsif
+            @player1.deck.cards[0].rank == @player2.deck.cards[0].rank && 
+            @player1.deck.cards.count >= 3 && @player2.deck.cards.count >= 3 &&
+            @player2.deck.cards[2].rank > @player1.deck.cards[2].rank
+            @player2
+        elsif
+            @player1.deck.cards[0].rank > @player2.deck.cards[0].rank
+            @player1
+        else
+            @player2.deck.cards[0].rank > @player1.deck.cards[0].rank
+            @player2
         end
     end
 
     def pile_cards
-        @spoils_of_war.push(@player1.deck.cards[0], @player2.deck.cards[0])
-        @player1.deck.cards.shift
-        @player2.deck.cards.shift
+        if type == :mutually_assured_destruction
+            @spoils_of_war.concat(@player1.deck.cards.shift(3))
+            @spoils_of_war.concat(@player2.deck.cards.shift(3))
+            @spoils_of_war.clear
+        elsif
+            type == :war
+            @spoils_of_war.concat(@player1.deck.cards.shift(3))
+            @spoils_of_war.concat(@player2.deck.cards.shift(3))
+        else type == :basic
+            @spoils_of_war.concat(@player1.deck.cards.shift(1)) 
+            @spoils_of_war.concat(@player2.deck.cards.shift(1))
+        end
     end
 
     def award_spoils(winner)
