6fea8a3c13e9beceff1c0bac513c6153d96bf3ca
Jono Sommers <jonosommers95@gmail.com>
2025-11-05 11:50:31 -0600

    all deck methods built, tested, and working.

--- lib/deck.rb
+++ lib/deck.rb
@@ -9,17 +9,20 @@ class Deck
   def rank_of_card_at(index)
     @cards[index].rank
   end
-
+  
   def high_ranking_cards
     @cards.select { |card| card.rank > 10 }
   end
 
   def percent_high_ranking
+    (high_ranking_cards.count.to_f / @cards.count.to_f * 100).round(2)
   end
 
   def remove_card
+    @cards.shift
   end
 
-  def add_card
+  def add_card(card)
+    @cards.push(card)
   end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,4 +1,5 @@
 require 'rspec'
+require 'pry'
 require './lib/card'
 require './lib/deck'
 
@@ -32,8 +33,35 @@ RSpec.describe Deck do
   end
 
   describe '#high_ranking_cards' do
-    it 'returns card with a rank above 10' do
+    it 'returns cards with a rank above 10' do
       expect(@deck.high_ranking_cards).to eq([@card1, @card3])
+    end
+  end
+
+  describe '#percent_high_ranking' do
+    it 'returns the percentage of high ranking cards in the deck' do
+      expect(@deck.percent_high_ranking).to eq(66.67)
+    end
+  end
+
+  describe '#remove_card' do
+    it 'removes and returns the first card in the deck' do
+      expect(@deck.remove_card).to eq(@card1)
+      expect(@deck.cards).to eq([@card2, @card3])
+      expect(@deck.high_ranking_cards).to eq([@card3])
+      expect(@deck.percent_high_ranking).to eq(50.00)
+    end
+  end
+
+  describe '#add_card' do
+    it 'adds a card to the end of the deck' do
+      card4 = Card.new(:club, '5', 5)
+
+      @deck.add_card(card4)
+
+      expect(@deck.cards).to eq([@card1, @card2, @card3, card4])
+      expect(@deck.high_ranking_cards).to eq([@card1, @card3])
+      expect(@deck.percent_high_ranking).to eq(50.00)
     end
   end
 end
